a45424490222fe1fac970203e620711f4cdd8885
TatriX <tatrics@gmail.com>
2019-06-11 12:18:51 +0200

    refactor: Extract todo to a separate file and change args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,67 @@
 //! Simple CLI TODO REPL app
 
-use serde::{Deserialize, Serialize};
-use std::fmt;
-use std::fs::File;
+mod todo;
+
 use std::io;
 use std::io::Write;
-use std::path::Path;
 
-#[derive(Deserialize, Serialize)]
-struct Task {
-    title: String,
-    completed: bool,
-}
+use todo::{Todo, Id};
 
-#[derive(Deserialize, Serialize, Default)]
-struct Todo {
-    tasks: Vec<Task>,
-}
-
-type Id = usize;
-
-impl Todo {
-    fn load(path: impl AsRef<Path>) -> io::Result<Self> {
-        let path = path.as_ref();
-        let file = File::open(path)?;
-        serde_json::from_reader(file).map_err(|err| {
-            eprintln!("Cannot deserialize from {:?}: {}", path, err);
-            io::ErrorKind::InvalidData.into()
-        })
-    }
-
-    fn save(&self, path: impl AsRef<Path>) -> io::Result<()> {
-        let path = path.as_ref();
-        let file = File::create(path)?;
-        serde_json::to_writer(file, self).map_err(|err| {
-            eprintln!("Cannot serialize to {:?}: {}", path, err);
-            io::ErrorKind::InvalidInput.into()
-        })
-    }
-
-    fn print(&self) {
-        println!("{}", self);
-    }
-
-    fn add(&mut self, title: impl ToString) {
-        self.tasks.push(Task {
-            title: title.to_string(),
-            completed: false,
-        });
-    }
-
-    fn mark_as_completed(&mut self, id: Id) {
-        self.tasks.get_mut(id).map(|todo| todo.completed = true);
-    }
-
-    fn mark_as_imcompleted(&mut self, id: Id) {
-        self.tasks.get_mut(id).map(|todo| todo.completed = false);
-    }
-
-    fn remove(&mut self, id: Id) {
-        if id < self.tasks.len() {
-            self.tasks.remove(id);
-        } else {
-            eprintln!("No such item: {}", id);
-        }
-    }
-}
-
-impl fmt::Display for Todo {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for (i, todo) in self.tasks.iter().enumerate() {
-            let status = if todo.completed { " DONE" } else { "" };
-            write!(f, "{}){} {}\n", i + 1, status, todo.title)?;
-        }
-        write!(f, "Total: {}", self.tasks.len())
-    }
-}
+const TASKS_PATH: &str = "tasks.json";
 
 fn main() -> io::Result<()> {
-    let tasks_path = "tasks.json";
-    let mut todo = Todo::load(tasks_path).unwrap_or_default();
+    let mut todo = Todo::load(TASKS_PATH).unwrap_or_default();
 
     todo.print();
     loop {
         print!("> ");
         io::stdout().flush()?; // force output
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
-        if input.is_empty() {
-            continue;
-        }
+        let input = &mut String::new();
+        io::stdin().read_line(input)?;
+
         let command = input.trim().splitn(2, ' ').collect::<Vec<_>>();
-        match command[0] {
-            "add" => {
-                todo.add(command[1]);
+        match command.as_slice() {
+            ["add", title] => {
+                todo.add(title);
                 todo.print();
             }
-            "remove" => {
-                parse_id(command[1]).map(|id| {
+            ["remove", id] => {
+                if let Some(id) = parse_id(id) {
                     todo.remove(id);
                     todo.print();
-                });
+                }
             }
-            "done" => {
-                parse_id(command[1]).map(|id| {
+            ["done", id] => {
+                if let Some(id) = parse_id(id) {
                     todo.mark_as_completed(id);
                     todo.print();
-                });
+                }
             }
-            "undo" => {
-                parse_id(command[1]).map(|id| {
+            ["undo", id] => {
+                if let Some(id) = parse_id(id) {
                     todo.mark_as_imcompleted(id);
                     todo.print();
-                });
+                }
             }
-            "exit" | "quit" | "bye" => {
+            ["exit"] | ["quit"] | ["bye"] => {
                 println!("Bye!");
                 break;
             }
             unknown => {
-                println!("Unknown command: {}", unknown);
+                println!("Unknown command: {:?}", unknown);
                 usage();
             }
         }
     }
 
-    todo.save(tasks_path)
+    todo.save(TASKS_PATH)
 }
 
-fn parse_id(s: &str) -> Option<usize> {
-    match s.parse::<usize>() {
+/// Parse Id from the string. Id is an index, so it's whatever number
+/// is parsed from the string minus one.
+fn parse_id(s: &str) -> Option<Id> {
+    match s.parse::<Id>() {
         Ok(id) => Some(id - 1),
         Err(err) => {
             eprintln!("Error parsing NUMBER: {}", err);
---
+++ src/todo.rs
@@ -0,0 +1,75 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::fs::File;
+use std::io;
+use std::path::Path;
+
+#[derive(Deserialize, Serialize)]
+struct Task {
+    title: String,
+    completed: bool,
+}
+
+#[derive(Deserialize, Serialize, Default)]
+pub struct Todo {
+    tasks: Vec<Task>,
+}
+
+pub type Id = usize;
+
+impl Todo {
+    pub fn load(path: impl AsRef<Path>) -> io::Result<Self> {
+        let path = path.as_ref();
+        let file = File::open(path)?;
+        serde_json::from_reader(file).map_err(|err| {
+            eprintln!("Cannot deserialize from {:?}: {}", path, err);
+            io::ErrorKind::InvalidData.into()
+        })
+    }
+
+    pub fn save(&self, path: impl AsRef<Path>) -> io::Result<()> {
+        let path = path.as_ref();
+        let file = File::create(path)?;
+        serde_json::to_writer(file, self).map_err(|err| {
+            eprintln!("Cannot serialize to {:?}: {}", path, err);
+            io::ErrorKind::InvalidInput.into()
+        })
+    }
+
+    pub fn print(&self) {
+        println!("{}", self);
+    }
+
+    pub fn add(&mut self, title: impl ToString) {
+        self.tasks.push(Task {
+            title: title.to_string(),
+            completed: false,
+        });
+    }
+
+    pub fn mark_as_completed(&mut self, id: Id) {
+        self.tasks.get_mut(id).map(|todo| todo.completed = true);
+    }
+
+    pub fn mark_as_imcompleted(&mut self, id: Id) {
+        self.tasks.get_mut(id).map(|todo| todo.completed = false);
+    }
+
+    pub fn remove(&mut self, id: Id) {
+        if id < self.tasks.len() {
+            self.tasks.remove(id);
+        } else {
+            eprintln!("No such item: {}", id);
+        }
+    }
+}
+
+impl fmt::Display for Todo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, todo) in self.tasks.iter().enumerate() {
+            let status = if todo.completed { " DONE" } else { "" };
+            write!(f, "{}){} {}\n", i + 1, status, todo.title)?;
+        }
+        write!(f, "Total: {}", self.tasks.len())
+    }
+}
